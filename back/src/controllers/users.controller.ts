08081bca3aa56b554fc76067475138774afdfb1e
Thibaut <thibaut.moncourtois@gmail.com>
2024-02-11 19:49:19 +0100

    ✨feat(back): create store, update and delete routes.

--- back/src/controllers/users.controller.ts
+++ back/src/controllers/users.controller.ts
@@ -1,14 +1,57 @@
 // Controller for users - users.controller.ts
 import { PrismaClient } from '@prisma/client'
+import type User from '@/types/User'
 
 const prisma = new PrismaClient()
 
 export const getUsers = async () => {
-  return await prisma.user.findMany()
+  return await prisma.users.findMany()
 }
 
 export const getUser = async (id: number) => {
-  return await prisma.user.findUnique({
+  return await prisma.users.findUnique({
+    where: {
+      id: id,
+    },
+  })
+}
+
+export const storeUser = async (user: User) => {
+  const { username, email, password, bio, avatar, role } = user
+
+  if (!password) return
+
+  return await prisma.users.create({
+    data: {
+      username,
+      email,
+      password,
+      bio,
+      avatar,
+      role,
+    },
+  })
+}
+
+export const updateUser = async (id: number, user: User) => {
+  const { username, email, password, bio, avatar, role } = user
+  return await prisma.users.update({
+    where: {
+      id,
+    },
+    data: {
+      username,
+      email,
+      password,
+      bio,
+      avatar,
+      role,
+    },
+  })
+}
+
+export const deleteUser = async (id: number) => {
+  return await prisma.users.delete({
     where: {
       id: id,
     },
