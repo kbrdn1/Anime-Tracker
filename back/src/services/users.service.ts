36b84a079de1cee9ac65955411b482574cd17191
kbrdn1 <kylianb1@icloud.com>
2024-02-16 21:49:51 +0100

    ✨ feat(back)!: add users filters

--- back/src/services/users.service.ts
+++ back/src/services/users.service.ts
@@ -4,11 +4,44 @@ import { PrismaClient } from '@prisma/client'
 class UsersService {
   private prisma = new PrismaClient()
 
-  public getAll = async () => {
-    // TODO: Add pagination
-    return await this.prisma.users.findMany()
+  public getAll = async (
+    filters: Record<string, string>,
+    limit?: number,
+    page?: number,
+    offset?: number,
+    orderBy?: string,
+    order?: string
+  ) => {
+    const { username, email, role } = filters
+
+    return await this.prisma.users.findMany({
+      where: {
+        AND: [
+          username ? { username: { contains: username } } : {},
+          email ? { email: { contains: email } } : {},
+          role ? { role: { contains: role } } : {},
+        ],
+      },
+      skip: offset ?? undefined,
+      orderBy: [{ [orderBy ?? 'created_at']: order ?? 'desc' }],
+      take: limit ?? undefined,
+    })
   }
-  
+
+  public count = async (filters: Record<string, string>) => {
+    const { username, email, role } = filters
+
+    return await this.prisma.users.count({
+      where: {
+        AND: [
+          username ? { username: { contains: username } } : {},
+          email ? { email: { contains: email } } : {},
+          role ? { role: { contains: role } } : {},
+        ],
+      }
+    })
+  }
+
   public get = async (id: number) => {
     return await this.prisma.users.findUnique({ where: { id } })
   }
@@ -21,7 +54,7 @@ class UsersService {
     return await this.prisma.users.update({ where: { id }, data })
   }
 
-  public delete = async (id: number) => {
+  public destroy = async (id: number) => {
     return await this.prisma.users.delete({ where: { id } })
   }
 }
