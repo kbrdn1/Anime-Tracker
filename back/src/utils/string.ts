7022e3eea67994370aa3c629eb4fa06cb1d62b7d
kbrdn1 <kylianb1@icloud.com>
2024-02-23 00:22:56 +0100

    ✨ feat(back)!: create string utils

---
+++ back/src/utils/string.ts
@@ -0,0 +1,12 @@
+// Utils for string manipulation - string.ts
+export const capitalizeFirstLetter = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+export const capitalizeFirstLetterOfEachPhrase = (str: string) => {
+  return str.replace(/(^\w{1}|\.\s*\w{1})/gi, (match) => match.toUpperCase())
+}
+
+export const capitalizeFirstLetterAndLetterAfterEachHyphen = (str: string) => {
+  return str.replace(/(^|-)(\w)/g, (match) => match.toUpperCase())
+}
