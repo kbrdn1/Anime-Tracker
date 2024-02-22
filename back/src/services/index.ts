12d6cb607b50c5ccae387d6733573ebf6c30a5c7
kbrdn1 <kylianb1@icloud.com>
2024-02-23 00:26:57 +0100

    🚩 feat(back)!: add statuses in default import services

--- back/src/services/index.ts
+++ back/src/services/index.ts
@@ -3,5 +3,12 @@ import authService from './auth.service'
 import usersService from './users.service'
 import authorsService from './authors.service'
 import gendersService from './genders.service'
+import statusesService from './statuses.service'
 
-export { authService, usersService, authorsService, gendersService }
+export {
+  authService,
+  usersService,
+  authorsService,
+  gendersService,
+  statusesService,
+}
