2d222916a38758749b6ba6ffa3b6d82d186e911d
kbrdn1 <kylianb1@icloud.com>
2024-02-24 00:25:11 +0100

    ✨ feat(back)!: implement security and brute force middlewares

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,4 +1,10 @@
+import { Server, env } from 'bun'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { csrf } from 'hono/csrf'
+import { secureHeaders } from 'hono/secure-headers'
+import { logger } from 'hono/logger'
+import { securityGuard, brutForceGuard } from './middlewares'
 import {
   authController,
   usersController,
@@ -9,14 +15,27 @@ import {
 
 const api = new Hono().basePath('/api/v1')
 
+api.use(cors())
+api.use(csrf())
+api.use(secureHeaders())
+api.use(logger())
+api.use(brutForceGuard)
+api.use('/*', securityGuard)
+
 api.get('/', c => {
   return c.text('Hello form Anime Tracker API !')
 })
 
-api.route('', usersController.router())
 api.route('', authController.router())
+api.route('', usersController.router())
 api.route('', authorsController.router())
 api.route('', gendersController.router())
 api.route('', statusesController.router())
 
-export default api
+export default {
+  port: env.PORT,
+  fetch(this, request: Request, server: Server) {
+    const ip = server.requestIP(request)
+    return api.fetch(request, { ip })
+  }
+}
