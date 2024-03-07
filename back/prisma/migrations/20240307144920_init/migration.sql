f909c11389367f4271e3698483d6b20f874df944
kbrdn1 <kylianb1@icloud.com>
2024-03-07 15:57:17 +0100

    🚩 feat (back)!: update schema and migrations

--- back/prisma/migrations/20240307144920_init/migration.sql
+++ back/prisma/migrations/20240307144920_init/migration.sql
@@ -237,6 +237,7 @@ CREATE TABLE `Themes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
