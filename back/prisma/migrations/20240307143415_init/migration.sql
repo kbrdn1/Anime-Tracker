b4fbd66046507ad3b81c83b2fb48329190437570
kbrdn1 <kylianb1@icloud.com>
2024-03-07 15:41:30 +0100

    🚩 feat (back)!: update schema and migrations

--- back/prisma/migrations/20240307143415_init/migration.sql
+++ back/prisma/migrations/20240307143415_init/migration.sql
@@ -255,6 +255,7 @@ CREATE TABLE `AnimeTypes` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `deleted_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
